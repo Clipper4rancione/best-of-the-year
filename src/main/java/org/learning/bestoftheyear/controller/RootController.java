f3c0fb5a7df6daf8a567c31d80a8d64903128b84
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-03-28 17:10:40 +0200

    [ADD] Push step 1 - 27/03/23

--- src/main/java/org/learning/bestoftheyear/controller/RootController.java
+++ src/main/java/org/learning/bestoftheyear/controller/RootController.java
@@ -1,6 +1,7 @@
 package org.learning.bestoftheyear.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RootController {
 
     @GetMapping
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("name", "Ciro");
         return "home";
     }
 }
